cb154eb97e682e648a64f43eb3277b5c0245ceb4
Betesega <betsega12345@gmail.com>
2024-07-12 16:27:50 +0300

    some static files and context updated and added

---
+++ src/data.js
@@ -0,0 +1,39 @@
+import Thumbnail1 from './images/blog1.jpg';
+import Thumbnail2 from './images/blog2.jpg';
+import Thumbnail3 from './images/blog3.jpg';
+import Thumbnail4 from './images/blog4.jpg';
+
+export const DUMMY_POSTS = [
+    {
+        id: '1',
+        thumbnail: Thumbnail1,
+        category: 'education',
+        title: "This is the title of the very first post on this blog",
+        desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat ad incidunt error quibusdam dolorem! Maiores error nesciunt corrupti dignissimos! Accusamus ullam at odit. Ut tenetur possimus error quibusdam necessitatibus recusandae!',
+        authorID: 3
+    },
+    {
+        id: '2',
+        thumbnail: Thumbnail2,
+        category: 'science',
+        title: "This is the title of the very first post on this blog",
+        desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat ad incidunt error quibusdam dolorem! Maiores error nesciunt corrupti dignissimos! Accusamus ullam at odit. Ut tenetur possimus error quibusdam necessitatibus recusandae!',
+        authorID: 1
+    },
+    {
+        id: '3',
+        thumbnail: Thumbnail3,
+        category: 'weather',
+        title: "This is the title of the very first post on this blog",
+        desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat ad incidunt error quibusdam dolorem! Maiores error nesciunt corrupti dignissimos! Accusamus ullam at odit. Ut tenetur possimus error quibusdam necessitatibus recusandae!',
+        authorID: 13
+    },
+    {
+        id: '4',
+        thumbnail: Thumbnail4,
+        category: 'farming',
+        title: "This is the title of the very first post on this blog",
+        desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat ad incidunt error quibusdam dolorem! Maiores error nesciunt corrupti dignissimos! Accusamus ullam at odit. Ut tenetur possimus error quibusdam necessitatibus recusandae!',
+        authorID: 3
+    }
+];
