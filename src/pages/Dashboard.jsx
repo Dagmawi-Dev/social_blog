758876ac17b78bd760ab53065018ccb58c6536ca
rahel <rabera121@gmail.com>
2024-07-14 18:20:16 +0300

    added

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,14 +5,13 @@ import { useState } from 'react';
 const Dashboard = () => {
   const currentUser = JSON.parse(localStorage.getItem('currentUser'));
   const dum = JSON.parse(localStorage.getItem('posts'));
-  const usher = dum.filter((d) => d.authorID === currentUser.authorID);//Filters the posts to only include those authored by the current user
-  const [posts, setPosts] = useState(usher);//Set Filtered Posts to State:
+  const usher = dum.filter((d) => d.authorID === currentUser.authorID);
+  const [posts, setPosts] = useState(usher);
 
   return (
     <section className="dashboard">
       {posts.length > 0 ? (
         <div className="container dashboard__container">
-          {/* Iterates over each post in the posts array and returns an article element for each post.*/}
           {posts.map((post) => {
             return (
               <article key={post.id} className="dashboard__post">
@@ -23,7 +22,7 @@ const Dashboard = () => {
                   <h5>{post.title}</h5>
                 </div>
                 <div className="dashboard__post-actions">
-                  {/* <Link to={`/posts/${post.id}`} className="btn sm">View</Link> */}
+                  <Link to={`/posts/${post.id}`} className="btn sm">View</Link>
                   <Link to={`/posts/${post.id}/edit`} className="btn sm primary">Edit</Link>
                   <Link to={`/posts/${post.id}/delete`} className="btn sm danger">Delete</Link>
                 </div>
